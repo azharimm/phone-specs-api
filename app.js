3210f1e44c27f066bf8c830e9bcf15c69d675e3b
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-01 13:06:00 +0700

    ^ base app structure

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const brandRoutes = require("./src/routes/brand");
+const specsRoutes = require("./src/routes/specs");
+
+app.get("/", (req, res) => {
+    res.json({
+        status: true,
+        data: "Phone Specs API",
+    });
+});
+
+app.use("/brand", brandRoutes);
+app.use("/specs", specsRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
