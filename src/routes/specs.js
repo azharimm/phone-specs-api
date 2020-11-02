8b159360d929457f36f5aa9b4f512ff33352a310
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-03 05:19:47 +0700

    - delete route & controller specs

--- src/routes/specs.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const specsController = require("../controllers/specsController");
-
-router.get("/", specsController.index);
-
-module.exports = router;
