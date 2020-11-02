70df0b5e4b98fcde7e58295141664369ac3443cc
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-03 05:06:17 +0700

    ^ list phones of brand

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const brandController = require("../controllers/brandController");
 
 router.get("/", brandController.index);
+router.get("/:brand_slug", brandController.show);
 
 module.exports = router;
