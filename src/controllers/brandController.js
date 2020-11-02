20d5d221070cd65513d21763e324628881a3643b
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-03 05:06:30 +0700

    ^ list phones of brand

--- src/controllers/brandController.js
+++ src/controllers/brandController.js
@@ -1,4 +1,5 @@
 const Brand = require("../models/Brand");
+const Specs = require("../models/Specs");
 const { json, errorJson } = require("../utils/response");
 
 exports.index = async (req, res) => {
@@ -22,3 +23,36 @@ exports.index = async (req, res) => {
         return errorJson(res, `Something went wrong: ${error}`);
     }
 };
+
+exports.show = async (req, res) => {
+    try {
+        const brand_slug = req.params.brand_slug;
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 10;
+        const skip = (page - 1) * limit;
+        const total = await Specs.find({ brand_slug }).countDocuments();
+        const brands = await Specs.find({ brand_slug })
+            .skip(skip)
+            .limit(parseInt(limit))
+            .exec();
+
+        return json(res, {
+            page: parseInt(page),
+            limit: parseInt(limit),
+            last_page: Math.ceil(total / limit),
+            phones: brands.map(brand => {
+                return {
+                    phone_name: brand.phone_name,
+                    phone_name_slug: brand.phone_name_slug,
+                    brand: brand.brand,
+                    brand_slug: brand.brand_slug,
+                    phone_img_url: brand.phone_name,
+                    created_at: brand.created_at
+                }
+            }),
+        });
+        
+    } catch (error) {
+        return errorJson(res, `Something went wrong: ${error}`);
+    }
+}
