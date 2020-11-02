df01c7d9d3126a741550e6b6983c454eab961234
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-03 05:20:17 +0700

    ^ route specs

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -5,5 +5,6 @@ const brandController = require("../controllers/brandController");
 
 router.get("/", brandController.index);
 router.get("/:brand_slug", brandController.show);
+router.get("/:brand_slug/:phone_name_slug", brandController.specs);
 
 module.exports = router;
