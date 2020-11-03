458155f775311a49985fd86adca4303188f7e3b4
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-03 07:06:33 +0700

    ^ search brand

--- src/controllers/phoneController.js
+++ src/controllers/phoneController.js
@@ -8,11 +8,12 @@ exports.index = async (req, res) => {
         const limit = req.query.limit || 10;
         const skip = (page - 1) * limit;
         const order = req.query.sort || 'brand:asc';
+        const search = req.query.search ? {brand: {$regex: req.query.search, $options: 'i'}} : {};
         const field = order.split(':')[0];
         const sort = order.split(':')[1] === 'asc' ? 1 : -1;
-
+        
         const total = await Brand.find().countDocuments();
-        const brands = await Brand.find({})
+        const brands = await Brand.find(search)
             .skip(skip)
             .limit(parseInt(limit))
             .sort([[field, sort]])
@@ -25,7 +26,7 @@ exports.index = async (req, res) => {
             brands,
         });
     } catch (error) {
-        return errorJson(res, `Something went wrong: ${error}`);
+        return errorJson(res, error);
     }
 };
 
@@ -38,7 +39,7 @@ exports.show = async (req, res) => {
         const order = req.query.sort || 'brand:asc';
         const field = order.split(':')[0];
         const sort = order.split(':')[1] === 'asc' ? 1 : -1;
-
+  
         const total = await Specs.find({ brand_slug }).countDocuments();
         const brands = await Specs.find({ brand_slug })
             .skip(skip)
@@ -49,6 +50,7 @@ exports.show = async (req, res) => {
         if(total === 0) {
             return errorJson(res, `Brand not found`, 404);
         }
+
 
         return json(res, {
             page: parseInt(page),
@@ -66,7 +68,7 @@ exports.show = async (req, res) => {
             }),
         });
     } catch (error) {
-        return errorJson(res, `Something went wrong: ${error}`);
+        return errorJson(res, error);
     }
 };
 
@@ -87,6 +89,6 @@ exports.specs = async (req, res) => {
             created_at: data.created_at
         });
     } catch (error) {
-        return errorJson(res, `${error}`);
+        return errorJson(res, error);
     }
 };
