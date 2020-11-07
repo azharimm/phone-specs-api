44e4c7679a883c2d8daa618dc2760641d7fb98e6
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-07 07:27:52 +0700

    ^ index page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,8 @@ const { json } = require("../utils/response");
 exports.index = (req, res) => {
     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     return json(res, {
+        maintainer: 'Azhari Muhammad M <azhari.marzan@gmail.com>',
+        source: 'https://github.com/azharimm/phone-specs-api',
         list_brands: {
             endpoint: '/brands',
             example: fullUrl+'brands'
