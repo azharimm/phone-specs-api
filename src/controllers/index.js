6894bab3cf28bde354b197643024f641e5a0909d
azharimm <azhari.marzan@gmail.com>
2021-09-05 06:29:49 +0700

    update: index v1

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,25 +5,6 @@ exports.index = (req, res) => {
     return json(res, {
         maintainer: 'Azhari Muhammad M <azhari.marzan@gmail.com>',
         source: 'https://github.com/azharimm/phone-specs-api',
-        list_brands: {
-            endpoint: '/brands',
-            example: fullUrl+'brands'
-        },
-        list_phones: {
-            endpoint: '/brands/{brand_slug}',
-            example: fullUrl+'brands/samsung'
-        },
-        phone_specs: {
-            endpoint: '/brands/{brand_slug}/{phone_slug}',
-            example: fullUrl+'brands/samsung/galaxy-note20-ultra-5g'
-        },
-        search: {
-            endpoint: '/search',
-            example: fullUrl+'search?query=Galaxy Note'
-        },
-        scrape: {
-            endpoint: '/scrape',
-            example: fullUrl+'scrape?url=https://www.gsmarena.com/samsung_galaxy_tab_s7+-10336.php'
-        }
+        v2: fullUrl+'v2'
     })
 }
