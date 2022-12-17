4a13ef17dd0e148e6573da4202606d2851c38d27
azharimm <azhari.marzan@gmail.com>
2022-12-17 07:49:44 +0700

    remove unnecessary slash

--- src/controllers/v2/index.js
+++ src/controllers/v2/index.js
@@ -8,31 +8,31 @@ exports.index = (req, res) => {
 		description: "Phone Specs API v2, Recommended to use",
 		list_brands: {
 			endpoint: "/brands",
-			example: fullUrl + "/brands",
+			example: fullUrl + "brands",
 		},
 		list_phones: {
 			endpoint: "/brands/{brand_slug}",
-			example: fullUrl + "/brands/apple-phones-48",
+			example: fullUrl + "brands/apple-phones-48",
 		},
 		phone_specs: {
 			endpoint: "/{phone_slug}",
-			example: fullUrl + "/apple_iphone_12_pro_max-10237",
+			example: fullUrl + "apple_iphone_12_pro_max-10237",
 		},
 		search: {
 			endpoint: "/search",
-			example: fullUrl + "/search?query=Iphone 12 pro max",
+			example: fullUrl + "search?query=Iphone 12 pro max",
 		},
 		latest: {
 			endpoint: "/latest",
-			example: fullUrl + "/latest",
+			example: fullUrl + "latest",
 		},
 		top_by_interest: {
 			endpoint: "/top-by-interest",
-			example: fullUrl + "/top-by-interest",
+			example: fullUrl + "top-by-interest",
 		},
 		top_by_fans: {
 			endpoint: "/top-by-fans",
-			example: fullUrl + "/top-by-fans",
+			example: fullUrl + "top-by-fans",
 		},
 	});
 };
