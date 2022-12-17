826897014df859c7abd493b81f5f486f6c440213
azharimm <azhari.marzan@gmail.com>
2022-12-17 07:27:12 +0700

    remove old api path

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const express = require("express");
 const app = express();
 // require("./src/db/connection");
 
-const indexRoutes = require("./src/routes/index");
-const indexRoutesV2 = require("./src/routes/v2/index");
+const indexRoutes = require("./src/routes/v2/index");
 const errorRoutes = require("./src/routes/error");
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+	next();
 });
 
 app.use("/", indexRoutes);
-app.use("/v2", indexRoutesV2);
-app.use('*', errorRoutes);
+app.use("*", errorRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
